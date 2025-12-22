921ad413c4233c44e376e345e2f917c0cdfd2950
smilemasa <stargazer.masa1220@gmail.com>
2025-12-22 11:11:23 +0900

    style: Remove global CSS file and its import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next"
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter"
 import { ClientProviders } from "../feature/components/ClientProviders"
 import { Layout } from "../feature/components/Layout"
-import "./globals.css"
 
 export const metadata: Metadata = {
   title: "Todo App",
