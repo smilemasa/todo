7dadcd3ccb8c9bba54cddbe9b8d6804f297fb269
smilemasa <stargazer.masa1220@gmail.com>
2025-12-31 12:16:04 +0900

    add icon.

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,14 +11,20 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/icon.png",
+        src: "/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icon.png",
+        src: "/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "maskable",
       },
     ],
   }
