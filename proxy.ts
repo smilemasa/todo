9b6762daeff072c4a1f349a7f4c7ac98d53cf71a
smilemasa <stargazer.masa1220@gmail.com>
2025-12-31 12:46:00 +0900

    feat: exclude manifest and PNG files from proxy paths and add execution environment documentation.

--- proxy.ts
+++ proxy.ts
@@ -37,6 +37,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    "/((?!login|api|_next/static|_next/image|favicon.ico).*)",
+    "/((?!login|api|_next/static|_next/image|favicon.ico|manifest.webmanifest|.*\\.png$).*)",
   ],
 }
